a81a0fc51bceafae3f5ed7a9cf2025f2a3b60bf4
alfamine <51228331+alfamine@users.noreply.github.com>
2025-06-13 14:36:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ client = openai.OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # Page settings
 st.set_page_config(page_title="Roswell-POC", page_icon="📘")
-logo = Image.open("igtlogo.png")
+logo = Image.open("IGT Logo.png")
 st.logo(logo, width=180)
 st.title("📘 Roswell-POC – Chatbot")
 st.subheader("Ask about Roswell, GA Code of Ordinances")
